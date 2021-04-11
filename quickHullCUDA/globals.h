35b6fe982330b0fce4b7aaa685f39226e1ca6a25
brav3t <bravetgm@gmail.com>
2021-04-11 18:51:32 +0200

    Add __file__ to CUDA_CALL macro

--- quickHullCUDA/globals.h
+++ quickHullCUDA/globals.h
@@ -7,8 +7,8 @@
 #define N1000 1000
 
 #define CUDA_CALL(x) do { if((x)!=cudaSuccess) { \
-    printf("Error at line:%d\n",__LINE__); exit(EXIT_FAILURE); } } while(0)
+    printf("\nError at %s line:%d\n",__FILE__,__LINE__); exit(EXIT_FAILURE); } } while(0)
 
 //#define CURAND_CALL(x) do { if((x)!=CURAND_STATUS_SUCCESS) { \
-//    printf("Error at %s:%d\n",__FILE__,__LINE__);\
+//    printf("Error at %s line:%d\n",__FILE__,__LINE__);\
 //    return EXIT_FAILURE;}} while(0)
