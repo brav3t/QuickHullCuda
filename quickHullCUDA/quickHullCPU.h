eca10e0ad27e014d10353930b4277e2aea48e0dc
brav3t <bravetgm@gmail.com>
2021-05-13 15:09:31 +0200

    Remove unused random gen and move func decls to header files

--- quickHullCUDA/quickHullCPU.h
+++ quickHullCUDA/quickHullCPU.h
@@ -2,4 +2,7 @@
 
 #include "globals.h"
 
+// Calculates distances and add element to hull if no farther element is found.
+void quickHull(int P1Idx_, int P2Idx_, int expectedSide_);
+// Main function to calcualte min and max values and start the recursion.
 void quickHullCPU(int* pointsX, int* pointsY);
--- quickHullCUDA/quickHullGPU.cuh
+++ quickHullCUDA/quickHullGPU.cuh
@@ -5,14 +5,24 @@
 
 #include "globals.h"
 
-#define BLOCK_SIZE 1024
+#define BLOCK_SIZE 32
 #define GRID_SIZE ((N - 1) / BLOCK_SIZE + 1)
 
 #define CUDA_CALL(x) do { if((x)!=cudaSuccess) { \
     printf("\nError at %s line:%d\n",__FILE__,__LINE__); exit(EXIT_FAILURE); } } while(0)
 
-//#define CURAND_CALL(x) do { if((x)!=CURAND_STATUS_SUCCESS) { \
-//    printf("Error at %s line:%d\n",__FILE__,__LINE__);\
-//    return EXIT_FAILURE;}} while(0)
+// Gets the minimum value from the array.
+__global__ static void MinSearch(int* min, int* array);
+__global__ static void MaxSearch(int* max, int* array);
+__global__ void getIndexOfValue(int* index, int* value, int* array);
+// Returns and calculates all points distances from P1-P2 line. If side is not correct then distance is zero.
+__global__ void getDistancesFromLine(int* distances, int* P1Idx, int* P2Idx, int* expectedSide, int* pointsX, int* pointsY);
+// Get the side of the point which has the max distance from P1-P2 line.
+__global__ void getSide(int* side, int* maxDistIdx, int* P1Idx, int* P2Idx, int* pointsX, int* pointsY);
+// If P1 or P2 is not in hull then add to it.
+__global__ void tryAddToHull(int* P1Idx, int* P2Idx, int* pointsX, int* pointsY);
 
+// Calculates distances and add element to hull if no farther element is found.
+void quickHull(int* d_P1Idx /*Left point of line*/, int* d_P2Idx /*Right point of line*/, int* d_expectedSide, int* d_pointsX, int* d_pointsY);
+// Main function to calcualte min and max values and start the recursion.
 void quickHullGPU(int* h_pointsX, int* h_pointsY);
